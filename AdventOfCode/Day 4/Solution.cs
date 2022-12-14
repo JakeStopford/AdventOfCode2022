77544a6673c10d881aa61f462c070c18c672b15b
JakeStopford <jake_stopford@hotmail.com>
2022-12-14 23:26:27 +0000

    completed day 4 part 2

--- AdventOfCode/Day 4/Solution.cs
+++ AdventOfCode/Day 4/Solution.cs
@@ -2,14 +2,14 @@ namespace AdventOfCode.Day4
 {
     public static class Solution4
     {
-        private static string[] backpacks =
+        private static string[] pairs =
             System.IO.File.ReadAllLines(@"C:\source\repos\AdventOfCode2023\AdventOfCode\Resources\Day4.txt");
 
         public static int GetTotalContainedPairs()
         {
             var total = 0;
 
-            var pairs = backpacks.Select(x => x.Split(","));
+            var pairs = Solution4.pairs.Select(x => x.Split(","));
             var numbers = pairs.Select(x => x.Select(y => y.Split("-"))).ToList();
 
             for (var i = 0; i < numbers.Count(); i++)
@@ -24,6 +24,30 @@ namespace AdventOfCode.Day4
                 else if (int.Parse(low.First()) <= int.Parse(low.Last()) && int.Parse(high.First()) >= int.Parse(high.Last()))
                 {
                     total++;
+                }
+            }
+            return total;
+        }
+
+        public static int GetTotalOverlappingPairs()
+        {
+            var total = 0;
+
+            var pairs = Solution4.pairs.Select(x => x.Split(","));
+            var numbers = pairs.Select(x => x.Select(y => y.Split("-"))).ToList();
+
+            for (var i = 0; i < numbers.Count(); i++)
+            {
+                var low = numbers.Select(x => x.Select(y => y[0])).ElementAt(i);
+                var high = numbers.Select(x => x.Select(y => y[1])).ElementAt(i);
+
+                for (var x = int.Parse(low.First()); x <= int.Parse(high.First()); x++)
+                {
+                    if (x >= int.Parse(low.Last()) && x <= int.Parse(high.Last()))
+                    {
+                        total++;
+                        break;
+                    }
                 }
             }
             return total;
--- AdventOfCode/Day 4/Tests.cs
+++ AdventOfCode/Day 4/Tests.cs
@@ -6,11 +6,19 @@ namespace AdventOfCode.Day4
     public class Tests
     {
         [Test]
-        public void TestTotalScore()
+        public void TestCompleteOverlap()
         {
             var actual = Solution4.GetTotalContainedPairs();
 
             actual.Should().Be(509);
+        }
+        
+        [Test]
+        public void TestAnyOverlap()
+        {
+            var actual = Solution4.GetTotalOverlappingPairs();
+
+            actual.Should().Be(870);
         }
     }
 }
